8c9b37bba7bcdc8adf70d0dc158160ecbb7ad4f2
Mikael <alochmar@gmail.com>
2025-11-06 00:39:48 +0200

    Cleanup.

--- 2025/2.py
+++ 2025/2.py
@@ -3,58 +3,47 @@ import re
 
 
 class Complex:
-    value = None
-
     def __init__(self, real, imag):
-        self.value = (real, imag)
+        self.real = real
+        self.imag = imag
 
     def __add__(self, other):
-        return Complex(self.value[0] + other[0], self.value[1] + other[1])
+        return Complex(self.real + other.real, self.imag + other.imag)
 
     def __mul__(self, other):
-        real = self.value[0] * other[0] - self.value[1] * other[1]
-        imag = self.value[0] * other[1] + self.value[1] * other[0]
-        return Complex(real, imag)
+        return Complex(self.real * other.real - self.imag * other.imag,
+                       self.real * other.imag + self.imag * other.real)
 
     def __truediv__(self, other):
-        real = int(self.value[0] / other[0])
-        imag = int(self.value[1] / other[1])
-        return Complex(real, imag)
+        return Complex(int(self.real / other.real),
+                       int(self.imag / other.imag))
 
-    def __getitem__(self, item):
-        return self.value[item]
+    def check(self, n, div):
+        r = Complex(0, 0)
+        for _ in range(n):
+            r *= r
+            r /= Complex(div, div)
+            r += self
+            if abs(r.real) > 1_000_000 or abs(r.imag) > 1_000_000:
+                return False
+        return r
 
-    def __str__(self):
-        return f'Complex({self.value[0]}, {self.value[1]})'
 
 part1 = open('inputs/2_1.txt').read()
 m = re.search(r'([0-9-]+),([0-9-]+)', part1)
-for name, a in ('test', Complex(25, 9)), ('1', Complex(int(m.group(1)), int(m.group(2)))):
-    r = Complex(0, 0)
-    for _ in range(3):
-        r *= r
-        r /= Complex(10, 10)
-        r += a
-    print(f'part {name}: [{r[0]},{r[1]}]')
+for name, a in (('test', Complex(25, 9)),
+                ('1', Complex(int(m.group(1)), int(m.group(2))))):
+    a = a.check(3, 10)
+    print(f'part {name}: [{a.real},{a.imag}]')
 
 part2 = open('inputs/2_2.txt').read()
 m = re.search(r'([0-9-]+),([0-9-]+)', part2)
 start = Complex(int(m.group(1)), int(m.group(2)))
-
-def work(pos):
-    r = Complex(0, 0)
-    for i in range(100):
-        r *= r
-        r /= Complex(100_000, 100_000)
-        r += pos
-        if abs(r[0]) > 1_000_000 or abs(r[1]) > 1_000_000:
-            return False
-    return r
-
-for name, d, length in (('part 2', 10, 101), ('part 3', 1, 1001)):
+for name, d, length in (('part 2', 10, 101),
+                        ('part 3', 1, 1001)):
     c = 0
     for row, col in itertools.product(range(length), range(length)):
         pos = start + Complex(col * d, row * d)
-        if work(pos):
+        if pos.check(100, 100_000):
             c += 1
-    print('%s %s' % (name, c))
+    print('%s: %s' % (name, c))
